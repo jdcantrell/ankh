0b22c08a53af74996961bb9d4e3327d24048be67
John Cantrell <jdcantrell@gmail.com>
2010-07-26 14:23:45 -0500

    Added simple Vimeo display output in to unicode.

--- stream.py
+++ stream.py
@@ -1,12 +1,14 @@
 import feedparser
 import re
+import codecs
 
 def load_feed(url, display):
+    print "Parsing feed at %s..." % url
     feed = feedparser.parse(url)
     s = []
     for entry in feed.entries[0:5]:
         s.append(display(entry))
-    return "".join(s)
+    return u''.join(s)
 
 
 #django simple template - based off http://effbot.org/zone/django-simple-template.htm
@@ -37,24 +39,40 @@ def variable(var, context):
     return load_feed(url,display_functions[function])
 #End shameless code cut 'n paste
 
-def display_simple(entry, start=False, end=False):
+def display_simple(entry):
     if entry.has_key('content'):
-        return  "<li>%s" % entry.content[0].value
-    return "<li>%s" % entry.title
+        return  u'<li>%s' % entry.content[0].value
+    return u'<li>%s' % entry.title
 
-def display_link(entry, start=False, end=False):
-    return "<li><a href='%s'>%s</a>" % (entry.link,entry.title)
+def display_link(entry):
+    return u'<li><a href="%s">%s</a>' % (entry.link,entry.title)
+
+def display_vimeo(entry):
+    return u'<object width="350" height="262"><param name="allowfullscreen" \
+        value="true" /><param name="allowscriptaccess" value="always" />\
+        <param name="movie" value="%s&amp;server=vimeo.com&amp;show_title=1\
+        &amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1"\
+        /><embed src="%s&amp;server=vimeo.com&amp;show_title=1&amp;\
+        show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1"\
+        type="application/x-shockwave-flash" allowfullscreen="true"\
+        allowscriptaccess="always" width="350" height="262"></embed>\
+        </object>' % (entry.enclosures[0].href, entry.enclosures[0].href);
 
 display_functions = {
     "simple": display_simple,
     "link": display_link,
+    "vimeo": display_vimeo,
 }
 
+
 def main():
-    f = open("stream.template")
+    f = codecs.open("stream.template", "r", "utf-8")
     template = f.read()
+    f.close()
     output = render(template, {})
-    print "".join(output)
+    outfile = codecs.open("stream.html","w","utf-8")
+    outfile.write(u''.join(output))
+
 
 if __name__ == "__main__":
     main()
