f5f8828971d48f0706a6c70f26a5f62ba9e2bfad
John Cantrell <jdcantrell@gmail.com>
2010-07-30 14:36:20 -0500

    Added a verbose option, by default no output is displayed.

--- ankh.py
+++ ankh.py
@@ -5,8 +5,9 @@ import codecs
 
 from ankh_displays import *
 
-def load_feed(url, display, count):
-    print "Parsing %s(%d entries)..." % (url, count)
+def load_feed(url, display, count, options):
+    if options.verbose:
+        print "Parsing %s(%d entries)..." % (url, count)
     feed = feedparser.parse(url)
     s = []
     for entry in feed.entries[0:count]:
@@ -14,14 +15,14 @@ def load_feed(url, display, count):
     return u''.join(s)
 
 #django simple template - based off http://effbot.org/zone/django-simple-template.htm
-def render(template, context):
+def render(template, context, options):
     next = iter(re.split("({{|}})", template)).next
     data = []
     try:
         token = next()
         while 1:
             if token == "{{": #variable
-                data.append(variable(next(), context))
+                data.append(variable(next(), context, options))
                 if next() != "}}":
                     raise SyntaxError("Missing variable terminator")
             else:
@@ -31,8 +32,7 @@ def render(template, context):
         pass
     return data
 
-def variable(var, context):
-    #print "Found %s" % var
+def variable(var, context, options):
     url = var.strip()
     function = "simple"
     count = 5
@@ -42,7 +42,7 @@ def variable(var, context):
             function, count = function.split("|",1)
             count = int(count.strip())
     
-    return load_feed(url, display_functions[function], count)
+    return load_feed(url, display_functions[function], count, options)
 #End shameless code cut 'n paste
 
 def main():
@@ -53,13 +53,16 @@ def main():
     parser.add_option("-o","--outfile", dest="outfile", \
         help="File to save the parsed template and feeds to. Defaults to ankh.html", \
         default="ankh.html")
+    parser.add_option("-v","--verbose", dest="verbose", \
+        help="Be verbose about what is going on", action="store_true",
+        default=False)
 
     options, args = parser.parse_args()
 
     template_file = codecs.open(options.template, "r", "utf-8")
     template = template_file.read()
     template_file.close()
-    output = render(template, {})
+    output = render(template, {}, options)
     outfile = codecs.open(options.outfile,"w","utf-8")
     outfile.write(u''.join(output))
 
