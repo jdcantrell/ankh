d403946219618e9d046b1dad6ca62efaa37a5307
John Cantrell <jdcantrell@gmail.com>
2010-07-30 14:35:41 -0500

    Remove closing embed tag

--- ankh_displays.py
+++ ankh_displays.py
@@ -9,15 +9,16 @@ def display_link(entry):
     return u'<li><a href="%s">%s</a>' % (entry.link, entry.title)
 
 def display_vimeo(entry):
-    return u'<li><object width="310" height="232"><param name="allowfullscreen" \
-        value="true" /><param name="allowscriptaccess" value="always" />\
-        <param name="movie" value="%s&amp;server=vimeo.com&amp;show_title=1\
-        &amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1"\
-        /><embed src="%s&amp;server=vimeo.com&amp;show_title=1&amp;\
-        show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1"\
-        type="application/x-shockwave-flash" allowfullscreen="true"\
-        allowscriptaccess="always" width="310" height="232"></embed>\
-        </object>' % (entry.enclosures[0].href, entry.enclosures[0].href);
+    return u'<li><object width="310" height="232">\
+<param name="allowfullscreen" value="true">\
+<param name="allowscriptaccess" value="always">\
+<param name="movie" value="%s&amp;server=vimeo.com&amp;show_title=1\
+&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1">\
+    <embed src="%s&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;\
+show_portrait=0&amp;color=&amp;fullscreen=1"\
+type="application/x-shockwave-flash" allowfullscreen="true"\
+allowscriptaccess="always" width="310" height="232">\
+</object>' % (entry.enclosures[0].href, entry.enclosures[0].href);
 
 def display_delicious(entry):
     if entry.has_key("summary"):
