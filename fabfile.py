c320f07abd771d6b0ef528418cbb1df891e1590a
JD Cantrell <jdcantrell@gmail.com>
2014-01-01 01:09:33 -0700

    template tweaks

--- fabfile.py
+++ fabfile.py
@@ -10,7 +10,7 @@ stream_dir = "/home/jd/Projects/ankh"
 
 def build():
   local("mkdir -p ../goodrobot.net/_site/stream/")
-  local("python ankh.py -t goodrobot.template.html -o ../goodrobot.net/_site/stream/index.html -v")
+  local("python ankh.py -t goodrobot.template.html -o index.html -v")
 
 @hosts('jd@goodrobot.net')
 def deploy():
