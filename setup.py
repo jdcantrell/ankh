fe03a83be8f3244cdda1e3499fce59fee17abb96
JD Cantrell <jcantrell@trulia.com>
2014-09-14 18:17:41 -0700

    Initial py-pi changes

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='ankh',
+  version='1.0.0',
+  description='An html generator for rss feeds',
+  url='http://github.com/jdcantrell/ankh',
+  author='JD Cantrell',
+  author_email='python@goodrobot.net',
+  license='GPLv3',
+  scripts=['bin/ankh'],
+  packages=['ankh'],
+  install_requires=[
+    'feedparser',
+    'jinja2',
+    'requests',
+  ],
+  zip_safe=False
+)
