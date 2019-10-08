43f8de74e3899fe7354ced573501dd9e011b346f
JD Cantrell <jd@goodrobot.net>
2019-10-08 09:26:19 -0700

    Update caching to use requests-cache

--- ankh/ankh.py
+++ ankh/ankh.py
@@ -1,5 +1,5 @@
-'''Ankh - will parse a template with {{http://somewhere/rss/feed|fn}} tags
-fetching the feeds and running the function on each item in the feed'''
+"""Ankh - will parse a template with {{http://somewhere/rss/feed|fn}} tags
+fetching the feeds and running the function on each item in the feed"""
 import os
 import codecs
 import time
@@ -12,6 +12,7 @@ from bs4 import BeautifulSoup
 from jinja2 import Environment, FileSystemLoader
 import feedparser
 import requests
+import requests_cache
 
 from .noa import noa
 
@@ -25,65 +26,27 @@ log_stream_handler = logging.StreamHandler(log_stream)
 logger.addHandler(log_stream_handler)
 
 
-def _get_cache_file(url, path):
-    md5 = hashlib.md5()
-    md5.update(url)
-
-    return '%s/%s' % (path, md5.hexdigest())
-
-
-def _load_url_from_cache(url, cache_path):
-    cache_file = _get_cache_file(url, cache_path)
-    data = None
-
-    if os.path.exists(cache_file):
-        cache_fh = open(cache_file, 'r')
-        data = cache_fh.read()
-        cache_fh.close()
-
-    return data
-
-
 def _load_url(url):
     try:
-        headers = {
-            'User-Agent': 'linux:net.goodrobot.ankh:v0.0.1 (by /u/jdcantrell)'
-        }
+        headers = {"User-Agent": "linux:net.goodrobot.ankh:v0.0.1 (by /u/jdcantrell)"}
         r = requests.get(url, timeout=35, headers=headers)
         return r.text
     except requests.exceptions.RequestException as e:
         logger.error("Could not fetch %s" % url)
         logger.error("Exception was raised: %r" % e)
 
-    return ''
-
-
-def _write_cache(url, text, cache_path):
-    cache_file = _get_cache_file(url, cache_path)
-
-    cache_fh = codecs.open(cache_file, "w", "utf-8")
-    cache_fh.write(text)
-    cache_fh.close()
+    return ""
 
 
 def _get_feed(url):
     global options
     data = None
 
-    if options.cache:
-        data = _load_url_from_cache(url, options.cache_path)
-
     if data is None:
         if options.verbose:
             print("Fetching: %s" % url)
 
         data = _load_url(url)
-
-        if options.cache:
-            _write_cache(url, data, options.cache_path)
-
-    elif options.verbose:
-        print("Using cached version of %s" % url)
 
     return feedparser.parse(data)
 
@@ -111,19 +74,19 @@ def _pretty_time(ago):
 
     if ago < 3600:
         time_length = 0
-        time_unit = 'New!'
+        time_unit = "New!"
     elif ago < 43200:
         time_length = int(round(ago / 3600))
-        time_unit = 'hour'
+        time_unit = "hour"
     elif ago < 2419200:
         time_length = int(round(ago / 86400))
-        time_unit = 'day'
+        time_unit = "day"
     else:
         time_length = int(round(ago / 2419200))
-        time_unit = 'month'
+        time_unit = "month"
 
     if time_length > 1:
-        time_unit += 's'
+        time_unit += "s"
 
     return (time_length, time_unit)
 
@@ -148,34 +111,34 @@ def get_weather(latlngs):
 
 
 def time_sort(urls, per_feed_count=1):
-        '''Display the entry with time posted prefixed'''
-        entries = []
-        time_list = {}
-        for url in urls:
-            feed = _get_feed(url)
+    """Display the entry with time posted prefixed"""
+    entries = []
+    time_list = {}
+    for url in urls:
+        feed = _get_feed(url)
 
-            if len(feed.entries):
-                feed_entries = feed.entries[:per_feed_count]
-                for entry in feed_entries:
-                    entry.feed_title = feed.feed.title.split('-')[0]
+        if len(feed.entries):
+            feed_entries = feed.entries[:per_feed_count]
+            for entry in feed_entries:
+                entry.feed_title = feed.feed.title.split("-")[0]
 
-                    pub = _get_date(entry)
+                pub = _get_date(entry)
 
-                    ago = time.mktime(time.localtime()) - time.mktime(pub)
-                    # unique key
-                    while ago in time_list:
-                        ago += .1
+                ago = time.mktime(time.localtime()) - time.mktime(pub)
+                # unique key
+                while ago in time_list:
+                    ago += 0.1
 
-                    time_list[ago] = True
+                time_list[ago] = True
 
-                    entry.time_raw = ago
-                    entry.time_length, entry.time_unit = _pretty_time(ago)
-                    entries.append(entry)
+                entry.time_raw = ago
+                entry.time_length, entry.time_unit = _pretty_time(ago)
+                entries.append(entry)
 
-        if len(entries):
-            return sorted(entries, key=lambda k: k.time_raw)
-        else:
-            return []
+    if len(entries):
+        return sorted(entries, key=lambda k: k.time_raw)
+    else:
+        return []
 
 
 def get_log():
@@ -183,11 +146,11 @@ def get_log():
 
 
 def find_images(html_string):
-    soup = BeautifulSoup(html_string, 'html.parser')
+    soup = BeautifulSoup(html_string, "html.parser")
 
     img_src = []
-    for img in soup.find_all('img'):
-        img_src.append(img.get('src'))
+    for img in soup.find_all("img"):
+        img_src.append(img.get("src"))
 
     return img_src
 
@@ -196,30 +159,27 @@ def parse(template, outfile, opts):
     global options
     options = opts
     if options.cache:
-        if not os.path.exists(options.cache_path):
-            os.makedirs(options.cache_path)
+        print("Caching requests")
+        requests_cache.install_cache("ankh_cache", backend="sqlite")
 
     full_path = os.path.abspath(template)
     path = os.path.dirname(full_path)
 
     opts.template_paths.append(path)
 
-    env = Environment(
-        loader=FileSystemLoader(opts.template_paths),
-        autoescape=True
-    )
+    env = Environment(loader=FileSystemLoader(opts.template_paths), autoescape=True)
 
-    env.globals['get_entries'] = get_entries
-    env.filters['find_link'] = find_link
+    env.globals["get_entries"] = get_entries
+    env.filters["find_link"] = find_link
 
-    env.globals['time_sort'] = time_sort
-    env.globals['images'] = find_images
+    env.globals["time_sort"] = time_sort
+    env.globals["images"] = find_images
 
-    env.globals['get_weather'] = get_weather
-    env.globals['log'] = get_log
+    env.globals["get_weather"] = get_weather
+    env.globals["log"] = get_log
 
     print("Loading %s" % template)
-    template = env.get_template(full_path.replace(path + '/', ""))
+    template = env.get_template(full_path.replace(path + "/", ""))
 
     print("Rendering...")
 
--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from setuptools import setup
 
 setup(
-    name='ankh',
-    version='1.0.0',
-    description='An html generator for rss feeds',
-    url='http://github.com/jdcantrell/ankh',
-    author='JD Cantrell',
-    author_email='python@goodrobot.net',
-    license='GPLv3',
-    scripts=['bin/ankh'],
-    packages=['ankh'],
+    name="ankh",
+    version="1.0.0",
+    description="An html generator for rss feeds",
+    url="http://github.com/jdcantrell/ankh",
+    author="JD Cantrell",
+    author_email="python@goodrobot.net",
+    license="GPLv3",
+    scripts=["bin/ankh"],
+    packages=["ankh"],
     install_requires=[
-        'feedparser',
-        'jinja2',
-        'beautifulsoup4',
-        'requests',
+        "feedparser",
+        "jinja2",
+        "beautifulsoup4",
+        "requests",
+        "requests-cache",
     ],
-    zip_safe=False
+    zip_safe=False,
 )
